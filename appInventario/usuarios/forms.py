6ceb1c95f42ab82cde029b9c3be209b5273a5463
luis.olave <luis.olave.guzman@outlool.com>
2021-02-07 23:15:53 -0300

    agregar estilos en form

--- appInventario/usuarios/forms.py
+++ appInventario/usuarios/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import ModelForm, EmailInput, TextInput
+from django.forms import ModelForm, EmailInput, TextInput, CheckboxInput, PasswordInput
 
 from usuarios.models import Usuarios
 
@@ -8,18 +8,28 @@ from usuarios.models import Usuarios
 class UsuarioForm(ModelForm):
     class Meta:
         model = Usuarios
-        fields = ['rut', 'username', 'nombres', 'apellidos', 'correo', 'user_administrador']
+        fields = [
+            'rut', 'username', 'nombres', 'apellidos', 'correo', 'user_administrador']
         widgets = {
-            'correo': EmailInput(attrs={'type': 'email', 'placeholder':'tuCorreo@mail.cl'}),
+            'correo': EmailInput(
+                attrs={
+                    'type': 'email',
+                    'placeholder':'tuCorreo@mail.cl',
+                    'class':'form-control'
+                }),
             'rut': TextInput(
                 attrs={
                     'placeholder':'Ejemplo: 12345678-8',
-                    'class':'form-control input-sm'
-                })
+                    'class':'form-control'
+                }),
+            'username':TextInput(attrs={'class':'form-control'}),
+            'nombres':TextInput(attrs={'class':'form-control'}),
+            'apellidos':TextInput(attrs={'class':'form-control'}),
         }
         labels = {
             'username':'Cuenta de usuario',
         }
+
 class createUserForm(UserCreationForm):
     class Meta:
         model = Usuarios
@@ -39,8 +49,16 @@ class createUserForm(UserCreationForm):
                 attrs={
                     'placeholder':'Ejemplo: 12345678-8',
                     'class':'form-control input-lg'
-                })
+                }),
+            'username': TextInput(attrs={'class':'form-control'}),
+            'nombres': TextInput(attrs={'class':'form-control'}),
+            'apellidos': TextInput(attrs={'class':'form-control'}),
+            'correo': TextInput(attrs={'class':'form-control'}),
+            'password1': PasswordInput(attrs={'type':'password', 'class':'form-control'}),
+            'password2': PasswordInput(attrs={'type':'password', 'class':'form-control'}),
+            'user_administrador': CheckboxInput(attrs={'type':'radio'})
         }
+
 
 class formResetPassword(UserCreationForm):
     class Meta:
@@ -49,3 +67,7 @@ class formResetPassword(UserCreationForm):
             'password1',
             'password2',
         )
+        widgets = {
+            'password1': forms.PasswordInput(),
+            'password2': forms.PasswordInput(),
+        }
